7bfb62b69c1334f6bf9dafc4fef1de052b2cf7da
Esteban Meza <meza.19@osu.edu>
2021-03-30 03:53:15 -0400

    removed unecsesary routs folder and undefined const

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -22,22 +23,21 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/api.js"));
+
 
 
 
 // HTML Routes
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 })
 
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/stats.html"));
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
 })
 
 // API-Routes
